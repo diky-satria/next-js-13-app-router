7c2e9852493f20575ebf250a973dd9353877a097
diky <diky.ramadnu@praweda.id>
2024-04-04 21:02:26 +0700

    install axios

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -7,10 +7,21 @@ const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest, res: Response) {
   try {
-    const data = await prisma.$queryRaw`select * from user`;
+    let page: number = Number(req.nextUrl.searchParams.get("page")) || 0;
+    let limit: number = Number(req.nextUrl.searchParams.get("limit")) || 10;
+    let search: string = req.nextUrl.searchParams.get("search") || "";
+    let search_db = search
+      ? `WHERE name LIKE '%${search}%' OR email LIKE '%${search}%'`
+      : "";
+    let offset = page * limit;
+    let total: any = await prisma.$queryRaw`SELECT count(*) as total from user`;
+    let total_page = Math.ceil(Number(total[0].total) / limit);
+
+    const data =
+      await prisma.$queryRaw`SELECT u.*, u.id as "key", d.name as division_name FROM user as u join division as d on u.divisionId = d.id`;
 
     return NextResponse.json(
-      { message: "Success", data: data },
+      { message: "Success", total: total_page, data: data },
       { status: 200 }
     );
   } catch (error) {
--- src/app/master/user/page.tsx
+++ src/app/master/user/page.tsx
@@ -1,18 +1,103 @@
 "use client";
 
-import CekRedux from "@/component/cekRedux";
 import Dashboard from "@/component/layout/dashboard";
-import React from "react";
-import { useSelector } from "react-redux";
+import styles from "./user.module.scss";
+import React, { useEffect, useState } from "react";
+import { Pagination, Table } from "antd";
+import type { TableColumnsType, TableProps } from "antd";
+import axios from "@/interceptor/axios";
+import Column from "antd/es/table/Column";
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  email: string;
+}
 
 export default function User() {
-  const { value } = useSelector((state: any) => state.counter);
+  const [data, setData] = useState<DataType[]>();
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: "No",
+      render: (text, record, index) => (currentPage - 1) * pageSize + index + 1,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      sorter: {
+        compare: (a, b) => a.email.localeCompare(b.email),
+        multiple: 3,
+      },
+    },
+  ];
+
+  // const data: DataType[] = [
+  //   {
+  //     key: "1",
+  //     name: "John Brown",
+  //     email: 98,
+  //   },
+  //   {
+  //     key: "2",
+  //     name: "Jim Green",
+  //     email: 98,
+  //   },
+  // ];
+
+  const pageSize = 2; // Number of rows per page
+  let currentPage = 1; // Initialize with the current page number
+
+  useEffect(() => {
+    getAllUser();
+  }, []);
+
+  const getAllUser = async () => {
+    try {
+      let response = await axios.get("/api/user");
+      console.log("response ", response);
+      setData(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const [page, setPage] = useState(1);
+
+  const onChange: TableProps<DataType>["onChange"] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
 
   return (
     <div>
       <Dashboard>
-        user {value}
-        <CekRedux />
+        <div className={styles.scss_container}>
+          <Table
+            columns={columns}
+            dataSource={data}
+            onChange={onChange}
+            pagination={{
+              showTotal: (total, range) => (
+                <>
+                  {range[0]}-{range[1]} of {total} items
+                </>
+              ),
+              onChange: (page) => {
+                currentPage = page; // Update the current page number
+              },
+              defaultPageSize: 2,
+              showSizeChanger: true,
+            }}
+            rowKey={(record, index): any => index}
+          />
+        </div>
       </Dashboard>
     </div>
   );
